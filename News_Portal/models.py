1b46e0a9d9704782a904af817a6883c0a380540f
DonVito <vitoss32@mail.ru>
2022-03-07 18:44:56 +0300

    Написал методы like и dislike D2_9

--- News_Portal/models.py
+++ News_Portal/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
+# Класс написан
 class Author(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
 
-
+# Класс написан
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -12,18 +14,40 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    rating = models.PositiveSmallIntegerField()
+    rating = models.IntegerField(default=0)
     type = models.BooleanField(default=False)
     time = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-    category = models.ManyToManyField(Category, throught='PostCategory')
+    category = models.ManyToManyField(Category, through='PostCategory')
 
+    def like(self):
+        self.rating += 1
+        self.save()
 
+    def dislike(self):
+        if self.rating > 0:
+            self.rating -= 1
+            self.save()
+
+# Класс написан
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-
+# Класс написан
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add=True)
+    rating = models.PositiveSmallIntegerField()
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        if self.rating > 0:
+            self.rating -= 1
+            self.save()
