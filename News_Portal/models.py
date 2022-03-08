4e054f9847aa4eb17a5433c3574ee483d2dc374d
DonVito <vitoss32@mail.ru>
2022-03-08 14:28:26 +0300

    Закончил работу. Добавил команды в Shell D2_9

--- News_Portal/models.py
+++ News_Portal/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 # Класс написан
 class Author(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        # Cуммарный рейтинг каждой статьи автора умножается на 3
+        postR = self.post_set.aggregate(postRating=Sum('rating'))
+        pR = 0
+        pR += postR.get('postRating')
+
+        # Суммарный рейтинг всех комментариев автора
+        comR = self.user.comment_set.aggregate(commentRating=Sum('rating'))
+        cR = 0
+        cR += comR.get('commentRating')
+
+        # Суммарный рейтинг всех комментариев к статьям автора
+        # compostR = self.user.post_set.aggregate(commentpostRating=Sum('rating'))
+        # cpR = 0
+        # cpR += compostR.get('commentpostRating')
+
+        self.rating = pR * 3 + cR #+ cpR
+        self.save()
 
 # Класс написан
 class Category(models.Model):
@@ -14,8 +34,14 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    rating = models.IntegerField(default=0)
-    type = models.BooleanField(default=False)
+    rating = models.SmallIntegerField(default=0)
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    CAT_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    )
+    type = models.CharField(max_length=2, choices=CAT_CHOICES, default=ARTICLE)
     time = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
@@ -26,9 +52,11 @@ class Post(models.Model):
         self.save()
 
     def dislike(self):
-        if self.rating > 0:
-            self.rating -= 1
-            self.save()
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return f'{self.text[0:123]}...'
 
 # Класс написан
 class PostCategory(models.Model):
@@ -37,17 +65,17 @@ class PostCategory(models.Model):
 
 # Класс написан
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=255)
+    text = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
-    rating = models.PositiveSmallIntegerField()
+    rating = models.SmallIntegerField(default=0)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def like(self):
         self.rating += 1
         self.save()
 
     def dislike(self):
-        if self.rating > 0:
-            self.rating -= 1
-            self.save()
+        self.rating -= 1
+        self.save()
