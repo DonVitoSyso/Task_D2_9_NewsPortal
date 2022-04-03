0e54ca0ce0c1b84b00b93dd8138c7a9544417b48
DonVito <vitoss32@mail.ru>
2022-04-03 18:06:23 +0300

    Подкорректировал форму для добавления новости

--- News_Portal/form.py
+++ News_Portal/form.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm, CharField
-from .models import Post, User, Author
+from django.forms import ModelForm, CharField, ModelChoiceField
+from .models import Post, User, Author, Category
 
 
 # Создаём модельную форму
@@ -7,6 +7,11 @@ class PostForm(ModelForm):
     # title = CharField(label='Заголовок')
     # text = CharField(label='Статья')
     # type = CharField(label='Тип')
+    # мы хотим чтобы нам выводило категория списком
+    category = ModelChoiceField(
+        queryset=Category.objects.all(),
+        label='Категория'
+    )
     # в класс мета, как обычно, надо написать модель, по которой будет строится форма и нужные нам поля. Мы уже делали что-то похожее с фильтрами.
     class Meta:
         model = Post
